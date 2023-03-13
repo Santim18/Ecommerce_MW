eeb426b483acf1aee4860e90193d72da3f336142
JOHAN SANTIAGO MARIN RIOS <johanmarin302678@correo.itm.edu.co>
2023-03-12 19:32:01 -0500

    Create database and class countries

---
+++ EcommerceMW/EcommerceMW/DAL/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceMW.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceMW.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options ) : base(options)
+        {
+
+        }
+
+        public DbSet<Country>Countries { get; set; }
+
+    }
+}
---
+++ EcommerceMW/EcommerceMW/DAL/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceMW.DAL.Entities
+{
+    public class Entity
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        public DateTime? CreatedDate { get; set; } //When is created register en BD, it shows when it was created
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
